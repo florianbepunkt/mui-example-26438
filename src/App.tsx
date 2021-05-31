ceb2fc6166c336200054d2edcbe7b2b7b2253f3c
Florian Bischoff <mail@florian-bischoff.de>
2021-05-31 15:16:07 +0200

    feat: create issue demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  TextField,
+  ThemeProvider
+} from "@material-ui/core";
+import { createTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ThemeProvider theme={theme}><TextField label="Filled" variant="filled" /></ThemeProvider>;
 }
 
 export default App;
